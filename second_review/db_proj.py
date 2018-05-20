be8e464860c2a3f01e86032d3a34ab690882a99f
Egor <ekolodin98@gmail.com>
2018-05-20 22:43:04 +0300

    PEP8

--- second_review/db_proj.py
+++ second_review/db_proj.py
@@ -2,31 +2,37 @@ import psycopg2
 import matplotlib.pyplot as plt
 
 
-def Create_table_students(host, db_name):
+def create_table_students(host, db_name):
     conn = psycopg2.connect(host=host, dbname=db_name)
     cursor = conn.cursor()
-    cursor.execute('CREATE TABLE students(ID SERIAL PRIMARY KEY, name VARCHAR(30), surname VARCHAR(30), age INT)')
+    cursor.execute("""
+        CREATE TABLE students(ID SERIAL PRIMARY KEY, name VARCHAR(30),
+        surname VARCHAR(30), age INT)
+        """)
     cursor.close()
     conn.commit()
     conn.close()
 
 
-def Create_table_marks(host, db_name):
+def create_table_marks(host, db_name):
     conn = psycopg2.connect(host=host, dbname=db_name)
     cursor = conn.cursor()
-    cursor.execute('CREATE TABLE marks(ID INT PRIMARY KEY ,mark_1 INT, mark_2 INT, mark_3 INT, mark_4 INT, mark_5 INT)')
+    cursor.execute("""
+        CREATE TABLE marks(ID INT PRIMARY KEY, mark_1 INT,
+        mark_2 INT, mark_3 INT, mark_4 INT, mark_5 INT)
+        """)
     cursor.close()
     conn.commit()
     conn.close()
 
 
-def Fill_table_students(data, host, db_name):
+def fill_table_students(data, host, db_name):
     conn = psycopg2.connect(host=host, dbname=db_name)
     cursor = conn.cursor()
 
     for i in data:
         cursor.execute("""
-            INSERT INTO students(name, surname, age) 
+            INSERT INTO students(name, surname, age)
             VALUES (%s, %s, %s)
             """, (i[0], i[1], i[2]))
     cursor.close()
@@ -34,13 +40,13 @@ def Fill_table_students(data, host, db_name):
     conn.close()
 
 
-def Fill_table_marks(data, host, db_name):
+def fill_table_marks(data, host, db_name):
     conn = psycopg2.connect(host=host, dbname=db_name)
     cursor = conn.cursor()
 
     for i in data:
         cursor.execute("""
-            INSERT INTO marks 
+            INSERT INTO marks
             VALUES (%s, %s, %s, %s, %s, %s)
             """, (i[0], i[1], i[2], i[3], i[4], i[5]))
     cursor.close()
@@ -48,8 +54,8 @@ def Fill_table_marks(data, host, db_name):
     conn.close()
 
 
-def Get_students(file):
-    with open(file) as f:
+def get_students(link):
+    with open(link) as f:
         data = []
         for i in f.read().split('\n'):
             student = []
@@ -60,8 +66,8 @@ def Get_students(file):
         return data
 
 
-def Get_marks(file):
-    with open(file) as f:
+def get_marks(link):
+    with open(link) as f:
         data = []
         for i in f.read().split('\n'):
             marks = []
@@ -71,10 +77,13 @@ def Get_marks(file):
         return data
 
 
-def Get_average(host, db_name):
+def get_average(host, db_name):
     conn = psycopg2.connect(host=host, dbname=db_name)
     cursor = conn.cursor()
-    cursor.execute('SELECT (mark_1 + mark_2 + mark_3 + mark_4 + mark_5) / 5 from marks')
+    cursor.execute("""
+        SELECT (mark_1 + mark_2 + mark_3 + mark_4 + mark_5) / 5
+        from marks
+        """)
     result = cursor.fetchall()
     cursor.close()
     conn.commit()
@@ -82,7 +91,7 @@ def Get_average(host, db_name):
     return [i[0] for i in result]
 
 
-def Show_hist(sample):
+def show_hist(sample):
     plt.figure(figsize=(10, 4))
     plt.hist(sample, bins=8, alpha=0.4, color='orange')
     plt.xlabel('average')
@@ -91,8 +100,8 @@ def Show_hist(sample):
     plt.show()
 
 
-Create_table_students('localhost', 'group_697')
-Create_table_marks('localhost', 'group_697')
-Fill_table_students(Get_students('students.txt'), 'localhost', 'group_697')
-Fill_table_marks(Get_marks('marks.txt'), 'localhost', 'group_697')
-Show_hist(Get_average('localhost', 'group_697'))
+create_table_students('localhost', 'group_697')
+create_table_marks('localhost', 'group_697')
+fill_table_students(get_students('students.txt'), 'localhost', 'group_697')
+fill_table_marks(get_marks('marks.txt'), 'localhost', 'group_697')
+show_hist(get_average('localhost', 'group_697'))
